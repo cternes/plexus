20c8fef418cbd19f4b52b20fcaceeab3f019b07c
cternes <github@slackspace.de>
2013-09-22 18:22:23 +0200

    Added background with parallax effect. Adjusted viewport.

--- js/levels.js
+++ js/levels.js
@@ -3,6 +3,9 @@ Quintus.Levels = function(Q) {
     //-------------LEVEL1---------------
     Q.scene("level1", function(stage) {
 
+        // Add in a repeater for a little parallax action
+        stage.insert(new Q.Repeater({ asset: "background-sky.png", speedX: 0.5, speedY: 0.5 }));
+
         // Add in a tile layer, and make it the collision layer
         stage.collisionLayer(new Q.TileLayer({
             dataAsset: 'level1.tmx',
@@ -16,6 +19,7 @@ Quintus.Levels = function(Q) {
         // Give the stage a moveable viewport and tell it
         // to follow the player.
         stage.add("viewport").follow(player);
+        stage.viewport.offsetY = 250; //move the camera a little bit to the top to avoid seeing empty space
 
         // Add in a couple of stars
         //stage.insert(new Q.Star({x: 700, y: 210}));
