91a2f8508c609fc09f289de47fcc72db68f99b4b
cternes <github@slackspace.de>
2013-10-11 22:11:44 +0200

    Fixed enemy positions

--- js/player.js
+++ js/player.js
@@ -57,7 +57,7 @@ Quintus.Player = function(Q) {
             }
             
             //DEBUG ONLY, show x position of player
-            //Q("UI.Text",1).items[0].p.label = "x:" + this.p.x + " y:" + this.p.y;
+            //Q("UI.Text",1).items[0].p.label = "x:" + Math.floor(this.p.x) + " y:" + Math.floor(this.p.y);
         }
     });
 
